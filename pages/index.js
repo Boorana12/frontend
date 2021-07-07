b0da7c0c841f85e497686efc800a1407d2ec5c36
Boorana12 <booboo_kub@hotmail.com>
2021-07-07 14:13:45 +0700

    v3

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import React from 'react';
+import { Button, Box, ButtonGroup, Checkbox } from '@material-ui/core';
 
 export default function Home() {
   return (
@@ -15,6 +17,28 @@ export default function Home() {
           <Link href="/about"><a> About</a></Link> |
           <Link href="/service"><a> Service</a></Link> |
           <Link href="/products/P001"><a> Products</a></Link>
+          <br />
+          <Button variant="contained" color="primary">
+            Hello World
+          </Button>
+          <br />
+          <Box component="span" m={1}>
+            <Button variant="contained" color="secondary">Button Box1</Button>
+          </Box>
+          <br />
+          <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+            <Button>One</Button>
+            <Button>Two</Button>
+            <Button>Three</Button>
+          </ButtonGroup>
+          <br />
+          <Checkbox
+            defaultChecked
+            color="primary"
+            inputProps={{ 'aria-label': 'secondary checkbox' }}
+          />
+          <br />
+          
         </center>
       </main>
     </div>
