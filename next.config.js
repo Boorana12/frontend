8146ebb6a02315ef7085de7b65fb13767d8ba74f
Boorana12 <booboo_kub@hotmail.com>
2021-07-21 13:03:17 +0700

    lte1

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
 }
+const withSass = require("@zeit/next-sass");
+
+module.exports = withSass({
+    webpack: config => {
+        config.node = {
+            fs: 'empty'
+        }
+        return config
+    }
+})
