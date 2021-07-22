3acdb3ec5608348d28d4b299338125870c85c9e3
Boorana12 <booboo_kub@hotmail.com>
2021-07-22 10:26:55 +0700

    v2

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import AdminContent from '../components/layouts/admin.content'
 import AdminHeader from '../components/layouts/admin.header'
+import AdminSidebar from '../components/layouts/admin.sidebar'
+
 const Home = () => {
     return (
         <div>
             <AdminHeader />
+            <AdminSidebar />
+            <AdminContent />
         </div>
+
     )
 }
 
